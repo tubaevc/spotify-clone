23a36921db8cf0d443356b57c4f3dfa3b5e859db
tuba evcil <tugbaevcil6060@gmail.com>
2023-10-09 16:35:09 +0300

    rigt bar finished in bottombar

--- src/components/BottomBar/Player.js
+++ src/components/BottomBar/Player.js
@@ -7,7 +7,12 @@ function Player() {
   const [audio, state, controls, ref] = useAudio({
     src: "https://www.soundhelix.com/examples/mp3/SoundHelix-Song-2.mp3",
   });
-  //const volumeIcon = useMemo(() => {}, [state.volume, state.muted]);
+  const volumeIcon = useMemo(() => {
+    if (state.volume === 0 || state.muted) return "volumeMuted";
+    if (state.volume > 0 && state.volume < 0.33) return "volumeLow";
+    if (state.volume >= 0.33 && state.volume < 0.66) return "volumeNormal";
+    return "volumeFull";
+  }, [state.volume, state.muted]);
   return (
     <div className="flex justify-between items-center h-full">
       <div className="min-w-[11.25rem] w-[30%]">sol</div>
@@ -60,16 +65,22 @@ function Player() {
         <button className="w-8 h-8 flex items-center justify-center text-white text-opacity-70 hover:text-opacity-100">
           <Icon size={16} name="device" />
         </button>
-        <button className="w-8 h-8 flex items-center justify-center text-white text-opacity-70 hover:text-opacity-100">
-          <Icon size={16} name="volumeFull" />
+        <button
+          onClick={controls[state.muted ? "unmute" : "mute"]}
+          className="w-8 h-8 flex items-center justify-center text-white text-opacity-70 hover:text-opacity-100"
+        >
+          <Icon size={16} name={volumeIcon} />
         </button>
         <div className="w-[5.813rem] max-w-full">
           <CustomRange
             step={0.01}
             min={0}
             max={1}
-            value={state?.volume}
-            onChange={(value) => controls.volume(value)}
+            value={state.muted ? 0 : state?.volume}
+            onChange={(value) => {
+              controls.unmute();
+              controls.volume(value);
+            }}
           />
         </div>
         <button className="w-8 h-8 flex items-center justify-center text-white text-opacity-70 hover:text-opacity-100">
